18b12edcf76ed0dd314920b819e529ac7237d282
ronghua <rowang@rowang-mn1.linkedin.biz>
2013-10-24 18:25:49 -0700

    php

--- helloworld.php
+++ helloworld.php
@@ -1,13 +1,21 @@
-<HTML>
-<HEAD>
+
 <TITLE> Hello World in PHP </TITLE>
 </HEAD>
 <BODY>
+
 <?
-// Hello world in PHP
- print("Hello World");
+
+print("Hello World<br>");
+
+// $HTTP_USER_AGENT and $REMOTE_ADDR are two of many evironment
+// variables in PHP.  Environent variables store information about
+// the user's and server's environment
+
+print("You are using $_SERVER[HTTP_USER_AGENT]<br>");
+print("Your Internet address is $_SERVER[REMOTE_ADDR]<br>");
+
+
 ?>
+
 </BODY>
 </HTML>
-
-
