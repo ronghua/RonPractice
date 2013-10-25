1560457ee2cedcc51edcbe469b89ab44646e1352
ronghua <rowang@rowang-mn1.linkedin.biz>
2013-10-24 18:31:02 -0700

    php

--- helloworld.php
+++ helloworld.php
@@ -14,7 +14,9 @@ print("Hello World<br>");
 print("You are using $_SERVER[HTTP_USER_AGENT]<br>");
 print("Your Internet address is $_SERVER[REMOTE_ADDR]<br>");
 
-
+$ip = $_SERVER['REMOTE_ADDR'];
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+print("You are at $details");
 ?>
 
 </BODY>
