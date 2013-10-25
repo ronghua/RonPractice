b568db9aa5d4d88558c5ea2c599c3691e00e2f2d
ronghua <ronghuawang@outlook.com>
2013-10-24 22:53:45 -0700

    add OAuth.php

--- helloworld.php
+++ helloworld.php
@@ -99,14 +99,21 @@ curl_close($ch);
 $response = json_decode($data);
 
 foreach($response->businesses as $business):
-        echo "<img border=0 src='".$business->image_url."'><br/>";
+        echo "<img border=0 src='".$business->photo_url."'><br/>";
         echo $business->name."<br/>";
-		echo $business->phone "</br>";
+        echo $business->address1."<br/>";
+        echo $business->city ."<br/>";
+        echo $business->state ."<br/>";
+        echo $business->zip ."<br/>";
+        echo $business->latitude ."<br/>";
+        echo $business->longitude ."<br/>";
+
         echo "<hr>";
     endforeach;
 
+
 // Print it for debugging
-print_r($response);
+//print_r($response);
 
 
 ?>
